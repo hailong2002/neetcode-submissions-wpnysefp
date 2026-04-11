a2fe4a9756ec2fd661a6b861e2790a81b8736a28
hailong2002 <103204476+hailong2002@users.noreply.github.com>
2026-04-11 23:32:47 +0700

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> results = new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String key = new String(charArray);
+            // for (Character c : charArray) {
+            //    key += c;
+            // }
+   
+            List<String> group = new ArrayList<>();
+            if (map.get(key) != null) {
+                group = map.get(key);
+            }
+            group.add(str);
+            map.put(key, group);
+        }
+
+        for (String key : map.keySet()) {
+            results.add(map.get(key));
+        }
+        return results;
+    }
+
+    
+} 
