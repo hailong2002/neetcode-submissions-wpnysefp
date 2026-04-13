7e559332850acbfa970206a947bc2cec42d33907
hailong2002 <103204476+hailong2002@users.noreply.github.com>
2026-04-13 15:02:47 +0700

    Add: two-integer-sum-ii - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-5.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int i = 0, j = 1;
+        while (i < numbers.length) {
+            if (j > numbers.length - 1) {
+                i++;
+                j = 0;
+            }
+            if (numbers[i] + numbers[j] == target) {
+                return new int[] {i+1, j+1};
+            } 
+            j++;
+        }
+        return new int[]{};
+    }
+}
